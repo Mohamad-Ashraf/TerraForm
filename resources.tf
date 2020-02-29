cba38169b56cab7859bf790c40957fa2013e2b46
Mohamad-Ashraf <eng.mohamad.ashraf.2017@gmail.com>
2020-02-29 08:49:58 +0200

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,21 +1,26 @@
 resource "azurerm_resource_group" "terra_proj" {
   location = "West US"
-  name = "terra_rg"
+  name     = "terra_rg"
 }
 
 resource "azurerm_virtual_network" "terra_virtual_network" {
-  address_space = ["10.0.0.0/16"]
-  location = "West US"
-  name = "terravirtnetwork"
-  resource_group_name = "${azurerm_resource_group.terra_proj.name}"
-  dns_servers = ["10.0.0.4", "10.0.0.5"]
+  address_space       = ["10.0.0.0/16"]
+  location            = "West US"
+  name                = "terravirtnetwork"
+  resource_group_name = "azurerm_resource_group.terra_proj.name"
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+}
 
-  subnet {
-    name = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  virtual_network_name = "azurerm_virtual_network.terra_virtual_network.name"
+  resource_group_name  = "azurerm_resource_group.terra_proj"
+  address_prefix       = "10.0.1.0/24"
+}
 
-  subnet {
-    name = "subnet2"
-    address_prefix = "10.0.2.0/24"
-  }
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
+  virtual_network_name = "azurerm_virtual_network.terra_virtual_network.name"
+  resource_group_name  = "azurerm_resource_group.terra_proj"
+  address_prefix       = "10.0.2.0/24"
+}
