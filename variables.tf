b86454f0c485fbd2bada3241f3cd5060db9f1f80
Mohamad-Ashraf <eng.mohamad.ashraf.2017@gmail.com>
2020-03-15 18:27:14 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "admin_password" {
+  description = ""
+}
+variable "subscription_id" {
+  description = ""
+}
+variable "client_id" {
+  description = ""
+}
+variable "client_secret" {
+  description = ""
+}
+variable "tenant_id" {
+  description = ""
+}
+variable "location" {
+  description = ""
+}
+variable "db_name" {
+  description = ""
+}
+variable "sql_admin_username" {
+  description = ""
+}
+variable "sql_password" {
+  description = ""
+}
+variable "resource_group_name" {
+  description = ""
+}
